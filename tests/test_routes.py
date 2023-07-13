44591bc3a80c422f6ddec86e3a38f54d2909b3a8
yapsylvia <yapsylvia@yahoo.com>
2023-07-13 06:29:07 -0400

    update accounts implemented

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -156,3 +156,31 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         # assert that the len() of the data is 5 (the number of accounts you created)
         self.assertEqual(len(data), 5)
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        # send a self.client.post() request to the BASE_URL with a json payload of test_account.serialize()
+        resp = self.client.post(
+            BASE_URL,
+            json=test_account.serialize(),
+            content_type="application/json"
+        )
+        # assert that the resp.status_code is status.HTTP_201_CREATED
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the account
+
+        # get the data from resp.get_json() as new_account
+        new_account = resp.get_json()
+        # change new_account["name"] to something known
+        new_account["name"] = "The new name"
+        # send a self.client.put() request to the BASE_URL with a json payload of new_account
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        # assert that the resp.status_code is status.HTTP_200_OK
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        # get the data from resp.get_json() as updated_account
+        updated_account = resp.get_json()
+        # assert that the updated_account["name"] is whatever you changed it to
+        self.assertEqual(updated_account["name"], "The new name")
