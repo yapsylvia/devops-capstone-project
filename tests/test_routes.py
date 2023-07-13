cd92a97829167baaa1bf93ae89733ef6d4538a28
yapsylvia <yapsylvia@yahoo.com>
2023-07-13 05:50:21 -0400

    list all accounts imlemented

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -145,3 +145,14 @@ class TestAccountService(TestCase):
         # assert that the resp.status_code is status.HTTP_404_NOT_FOUND
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        # send a self.client.get() request to the BASE_URL
+        resp = self.client.get(f"{BASE_URL}")
+        # assert that the resp.status_code is status.HTTP_200_OK
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        # get the data from resp.get_json()
+        data = resp.get_json()
+        # assert that the len() of the data is 5 (the number of accounts you created)
+        self.assertEqual(len(data), 5)
